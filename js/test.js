57c57f210cb27bba7e026aa2c0e1d19b889dfd25
mkaanilhan <kaan.ilhan@proton.me>
2024-06-12 14:42:15 +0300

    11.07 folder structore and ignore

---
+++ js/test.js
@@ -0,0 +1,9 @@
+import b, { list as arrayList, firstName as name, run as running } from '../js/imported.js'
+
+
+b()
+
+
+//console.log(firstName)
+
+
--- test.js
+++
@@ -1,9 +0,0 @@
-import b, { list as arrayList, firstName as name, run as running } from './imported.js'
-
-
-b()
-
-
-//console.log(firstName)
-
-
